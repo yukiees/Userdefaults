9e562cd453c1b2d32023df35ebd19a22398ac651
yukiees <yukiii8989@gmail.com>
2021-02-05 17:02:26 +0900

    userDefautls

--- Userdefaults/ViewController.swift
+++ Userdefaults/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var text = String()
+    var loadText = String()
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func preserve(_ sender: Any) {
+        text = textField.text!
+        saveData.set(text,forKey: "string")
+    }
+    
+    @IBAction func load(_ sender: Any) {
+        loadText = saveData.object(forKey: "string") as! String
+        Label.text = loadText
+    }
+    
+    
 }
 
